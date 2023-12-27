e8f07eb455350d2621e8468bebd142e78c239780
NagarajLLL <NagarajLLL>
2023-12-27 06:11:53 +0530

    commit

--- createinstance.tf
+++ createinstance.tf
@@ -1,17 +1,17 @@
-resource "aws_instance" "web5" {
+resource "aws_instance" "web7" {
   ami           = var.ami
   instance_type = var.instance_type
 
   tags = {
-    Name = "ion-instance5"
+    Name = "ion-instance7"
   }
 }
 
-resource "aws_instance" "web6" {
+resource "aws_instance" "web8" {
   ami           = var.ami
   instance_type = var.instance_type
 
   tags = {
-    Name = "ion-instance6"
+    Name = "ion-instance8"
   }
 }
